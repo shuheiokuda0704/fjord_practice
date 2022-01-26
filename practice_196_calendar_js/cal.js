4f06836bc5fee722059fe0fcc07c47dc0b004ddc
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-26 09:19:56 +0900

    Show calendar

--- practice_196_calendar_js/cal.js
+++ practice_196_calendar_js/cal.js
@@ -3,13 +3,29 @@ const argv = require('minimist')(process.argv.slice(2));
 const year = argv['y'] || now.getFullYear()
 const month = argv['m'] || now.getMonth() + 1
 
-function show_month_and_year() {
-    console.log((month + '').padStart(6, ' ') + "月 " + year);
+function showMonthAndYear() {
+    console.log(((month) + '').padStart(6, ' ') + "月 " + year);
 }
 
-function show_day_of_week() {
+function showDayOfWeek() {
     console.log('日 月 火 水 木 金 土');
 }
 
-show_month_and_year()
-show_day_of_week()
+function showDays() {
+    firstDate = new Date(year, month - 1, 1);
+    lastDate = new Date(year, month, 0);
+
+    process.stdout.write(('').padStart(3 * (firstDate.getDay()), ' '));
+    
+    for(let i = 1; i <= lastDate.getDate(); i++) {
+        process.stdout.write((i + '').padStart(2, ' ') + ' ');
+        if (new Date(year, month - 1, i).getDay() === 6) { // Saturday
+            console.log('');
+        }
+    }
+    console.log('');
+}
+
+showMonthAndYear();
+showDayOfWeek();
+showDays();
