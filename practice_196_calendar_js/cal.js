89779b4be36bdb505f9fc97e877ad602e8e3bd9b
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-01-26 09:34:14 +0900

    Fix eslint detected issues

--- practice_196_calendar_js/cal.js
+++ practice_196_calendar_js/cal.js
@@ -1,31 +1,31 @@
-const now  = new Date();
-const argv = require('minimist')(process.argv.slice(2));
-const year = argv['y'] || now.getFullYear()
-const month = argv['m'] || now.getMonth() + 1
+const argv = require('minimist')(process.argv.slice(2))
+const now = new Date()
+const year = argv.y || now.getFullYear()
+const month = argv.m || now.getMonth() + 1
 
-function showMonthAndYear() {
-    console.log(((month) + '').padStart(6, ' ') + "月 " + year);
+function showMonthAndYear () {
+  console.log(((month) + '').padStart(6, ' ') + '月 ' + year)
 }
 
-function showDayOfWeek() {
-    console.log('日 月 火 水 木 金 土');
+function showDayOfWeek () {
+  console.log('日 月 火 水 木 金 土')
 }
 
-function showDays() {
-    firstDate = new Date(year, month - 1, 1);
-    lastDate = new Date(year, month, 0);
+function showDays () {
+  const firstDate = new Date(year, month - 1, 1)
+  const lastDate = new Date(year, month, 0)
 
-    process.stdout.write(('').padStart(3 * (firstDate.getDay()), ' '));
-    
-    for(let i = 1; i <= lastDate.getDate(); i++) {
-        process.stdout.write((i + '').padStart(2, ' ') + ' ');
-        if (new Date(year, month - 1, i).getDay() === 6) { // Saturday
-            console.log('');
-        }
+  process.stdout.write(('').padStart(3 * (firstDate.getDay()), ' '))
+
+  for (let i = 1; i <= lastDate.getDate(); i++) {
+    process.stdout.write((i + '').padStart(2, ' ') + ' ')
+    if (new Date(year, month - 1, i).getDay() === 6) { // Saturday
+      console.log('')
     }
-    console.log('');
+  }
+  console.log('')
 }
 
-showMonthAndYear();
-showDayOfWeek();
-showDays();
+showMonthAndYear()
+showDayOfWeek()
+showDays()
